ae869f925ec53d5224107a895395f60616bc0b66
Kyle Kilat <kkilat@users.noreply.github.com>
2023-12-10 13:48:43 -0800

    chore: update placeholder script w/ separate WF and standalone examples

--- my-lib.js
+++ my-lib.js
@@ -1,5 +1,19 @@
+// If custom code relies on the Webflow.js or jQuery libraries loaded by Webflow,
+// it should be wrapped in a function that is pushed to the Webflow global array.
+// This ensures that the code will be run AFTER the Webflow provided libraries.
+// --------
+window.Webflow ||= [];
+window.Webflow.push(printHelloWebflow);
+
+function printHelloWebflow() {
+  console.log('Hello Webflow!');
+}
+
+// If custom code does not rely on the Webflow.js or jQuery libraries loaded by Webflow,
+// it can be run immediately, as a standalone script.
+// --------
+printHelloWorld();
+
 function printHelloWorld() {
   console.log('Hello world!');
 }
-
-printHelloWorld();
